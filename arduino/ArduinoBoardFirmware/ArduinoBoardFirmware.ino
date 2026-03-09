4aed6eb6350146d0eb49ad5f264dc8508ea9bc4e
Eduardo Rueda de Santiago <115037378+Eduardo-Rueda-de-Santiago@users.noreply.github.com>
2026-03-09 11:13:52 +0100

    Adjusted the example codes in the arduino firmware

--- arduino/ArduinoBoardFirmware/ArduinoBoardFirmware.ino
+++ arduino/ArduinoBoardFirmware/ArduinoBoardFirmware.ino
@@ -16,16 +16,13 @@
 
 LedController led1(LED_ID(1), PIN_LED_1, LED_NUM_PIXELS);
 LedController led2(LED_ID(2), PIN_LED_2, LED_NUM_PIXELS);
-// LedController led3("LED_3", PIN_LED_3);
 
-// LcdController lcd1("LCD_1", LCD_I2C_ADDRESS, LCD_COLS, LCD_ROWS);
-// LcdController lcd2("LCD_2", LCD_I2C_ADDRESS, LCD_COLS, LCD_ROWS);
-// LcdController lcd3("LCD_3", LCD_I2C_ADDRESS, LCD_COLS, LCD_ROWS);
+// LcdController lcdExample("LCD_1", LCD_I2C_ADDRESS, LCD_COLS, LCD_ROWS);
 
 BuzzerController buzzer1(BUZZER_ID(1), PIN_BUZZER_1);
 BuzzerController buzzer2(BUZZER_ID(2), PIN_BUZZER_2);
 
-// MotorController motor1("MOT_1", PIN_MOTOR_1_IN1, PIN_MOTOR_1_IN2, PIN_MOTOR_1_EN);
+// MotorController motorExample("MOT_1", PIN_MOTOR_1_IN1, PIN_MOTOR_1_IN2, PIN_MOTOR_1_EN);
 
 UltrasoundController us1(ULTRASOUND_ID(1), ULTRASOUND_1_TRIG_PIN, ULTRASOUND_1_ECHO_PIN);
 UltrasoundController us2(ULTRASOUND_ID(2), ULTRASOUND_2_TRIG_PIN, ULTRASOUND_2_ECHO_PIN);
@@ -49,14 +46,13 @@ void setup()
     coordinator.Attach(&led1, LED_BASE_ID);
     coordinator.Attach(&led2, LED_BASE_ID);
 
-    // coordinator.Attach(&lcd1, "LCD");
-    // coordinator.Attach(&lcd2, "LCD");
-    // coordinator.Attach(&lcd3, "LCD");
+    // coordinator.Attach(&lcdExample, "LCD");
 
     coordinator.Attach(&buzzer1, BUZZER_BASE_ID);
     coordinator.Attach(&buzzer2, BUZZER_BASE_ID);
 
-    // coordinator.Attach(&motor1, "MOT");
+    // coordinator.Attach(&motorExample, "MOT");
+
     coordinator.Attach(&us1, ULTRASOUND_BASE_ID);
     coordinator.Attach(&us2, ULTRASOUND_BASE_ID);
 
@@ -89,16 +85,7 @@ void loop()
 
     coordinator.readAndRoute();
 
-    // Periodic ultrasound measurement every 500 ms
-   /* static unsigned long lastMeasure = 0;
-    unsigned long now = millis();
-    if (now - lastMeasure >= 500)
-    {
-        lastMeasure = now;
-        us1.measure();
-        us2.measure();
-    }
-*/
     eyeLeft.redraw();
     eyeRight.redraw();
+
 }
